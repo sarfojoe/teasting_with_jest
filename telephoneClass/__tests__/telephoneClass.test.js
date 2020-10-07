b720839284383dad37b5417139fc90def9569e17
sarfojoe <60213446+sarfojoe@users.noreply.github.com>
2020-10-07 20:41:48 +0300

    test getAllNumbers

--- telephoneClass/__tests__/telephoneClass.test.js
+++ telephoneClass/__tests__/telephoneClass.test.js
@@ -55,6 +55,8 @@ describe("Test getTypes", () => {
 });
 
 describe("test getNumbersByType", () => {
+  let phoneRegister;
+
   beforeEach(() => {
     phoneRegister = new PhoneRegister(phones);
   });
@@ -69,5 +71,221 @@ describe("test getNumbersByType", () => {
         "12345678",
       ]);
     });
+
+    test('work numbers of Matt River return ["32121367","7635462"]', () => {
+      expect(
+        phoneRegister.getNumbersByType("Matt", "River", "work").sort()
+      ).toEqual(["32121367", "7635462"].sort());
+    });
+  });
+
+  describe("wrong type or wrong name returns an empty array", () => {
+    beforeEach(() => {
+      phoneRegister = new PhoneRegister(phones);
+    });
+
+    const testValues = [
+      ["Leila", "Hökki", "x", []],
+      ["Leila", "x", "home", []],
+      ["x", "Hökki", "home", []],
+      ["x", "y", "home", []],
+      ["x", "y", "z", []],
+    ];
+
+    test.each(testValues)(
+      'getNumbersByType("%s", "%s", "%s") returns an empty array %s',
+      (firstname, lastname, type, expected) => {
+        expect(
+          phoneRegister.getNumbersByType(firstname, lastname, type)
+        ).toEqual(expected);
+      }
+    );
+  });
+
+  describe('missing parameter throws an exception "missing parameter"', () => {
+    beforeEach(() => {
+      phoneRegister = new PhoneRegister(phones);
+    });
+
+    test("type missing", () => {
+      expect(() => phoneRegister.getNumbersByType("Leila", "Hökki")).toThrow(
+        "missing parameter"
+      );
+    });
+
+    test("lastname and type are missing", () => {
+      expect(() => phoneRegister.getNumbersByType("Leila")).toThrow(
+        "missing parameter"
+      );
+    });
+
+    test("all parameters missing", () => {
+      expect(() => phoneRegister.getNumbersByType()).toThrow(
+        "missing parameter"
+      );
+    });
+  });
+});
+
+describe("Test getAllNumbersByType", () => {
+  let phoneRegister;
+
+  beforeEach(() => {
+    phoneRegister = new PhoneRegister(phones);
+  });
+
+  test("method getAllNumbersByType is defined", () => {
+    expect(phoneRegister.getAllNumbersByType).toBeDefined();
+  });
+
+  test("get all numbers of type home", () => {
+    const expectedResult = [
+      {
+        firstname: "Leila",
+        lastname: "Hökki",
+        number: { type: "home", tel: "12345678" },
+      },
+      {
+        firstname: "Matt",
+        lastname: "River",
+        number: { type: "home", tel: "56834923" },
+      },
+    ];
+    expect(phoneRegister.getAllNumbersByType("home")).toEqual(expectedResult);
+  });
+
+  test("get all numbers of type work", () => {
+    const expectedResult = [
+      {
+        firstname: "Leila",
+        lastname: "Hökki",
+        number: { type: "work", tel: "87654321" },
+      },
+      {
+        firstname: "Matt",
+        lastname: "River",
+        number: { type: "work", tel: "32121367" },
+      },
+      {
+        firstname: "Matt",
+        lastname: "River",
+        number: { type: "work", tel: "7635462" },
+      },
+    ];
+    expect(phoneRegister.getAllNumbersByType("work")).toEqual(expectedResult);
+  });
+
+  test("get all numbers of type x will return an empty array []", () => {
+    expect(phoneRegister.getAllNumbersByType("x")).toEqual([]);
+  });
+
+  test('missing type throws an exception "missing parameter"', () => {
+    expect(() => phoneRegister.getAllNumbersByType()).toThrow(
+      "missing parameter"
+    );
+  });
+});
+
+describe("Test getAllNumbers", () => {
+  test("method getAllNumbers is defined", () => {
+    const phoneRegister = new PhoneRegister(phones);
+    expect(phoneRegister.getAllNumbers).toBeDefined();
+  });
+
+  test("get all numbers from the default data", () => {
+    const phoneRegister = new PhoneRegister(phones);
+    expect(phoneRegister.getAllNumbers()).toEqual(phones);
+  });
+
+  test("some phones are missing", () => {
+    const testData = [
+      {
+        firstname: "Leila",
+        lastname: "Hökki",
+        phones: [
+          { type: "home", number: "12345678" },
+          { type: "work", number: "87654321" },
+          { type: "mobile", number: "05040302" },
+        ],
+      },
+      {
+        firstname: "Matt",
+        lastname: "River",
+        phones: [],
+      },
+    ];
+
+    const expectedResult = [
+      {
+        firstname: "Leila",
+        lastname: "Hökki",
+        phones: [
+          { type: "home", number: "12345678" },
+          { type: "work", number: "87654321" },
+          { type: "mobile", number: "05040302" },
+        ],
+      },
+    ];
+
+    const phoneRegister = new PhoneRegister(testData);
+
+    expect(phoneRegister.getAllNumbers()).toEqual(expectedResult);
+  });
+
+  test("all phones are missing", () => {
+    const testData = [
+      {
+        firstname: "Leila",
+        lastname: "Hökki",
+        phones: [],
+      },
+      {
+        firstname: "Matt",
+        lastname: "River",
+        phones: [],
+      },
+    ];
+
+    const phoneRegister = new PhoneRegister(testData);
+
+    expect(phoneRegister.getAllNumbers()).toEqual([]);
+  });
+});
+
+describe("Test getName", () => {
+  let phoneRegister;
+
+  beforeEach(() => (phoneRegister = new PhoneRegister(phones)));
+
+  test("method getName is defined", () => {
+    expect(phoneRegister.getName).toBeDefined();
+  });
+
+  test('get the name of number "12345678" return {"firstname":"Leila", "lastname":"Hökki"}', () => {
+    expect(phoneRegister.getName("12345678")).toEqual({
+      firstname: "Leila",
+      lastname: "Hökki",
+    });
+  });
+
+  const testValues = [
+    ["12345678", { firstname: "Leila", lastname: "Hökki" }],
+    ["87654321", { firstname: "Leila", lastname: "Hökki" }],
+    ["05040302", { firstname: "Leila", lastname: "Hökki" }],
+    ["56834923", { firstname: "Matt", lastname: "River" }],
+    ["32121367", { firstname: "Matt", lastname: "River" }],
+    ["7635462", { firstname: "Matt", lastname: "River" }],
+  ];
+
+  test.each(testValues)("number %s returns %p", (number, expectedValue) => {
+    expect(phoneRegister.getName(number)).toEqual(expectedValue);
+  });
+
+  test("wrong number", () => {
+    expect(phoneRegister.getName("0000")).toBeNull();
+  });
+
+  test("missing parameter", () => {
+    expect(phoneRegister.getName()).toBeNull();
   });
 });
--- telephoneClass/telephoneClass.js
+++ telephoneClass/telephoneClass.js
@@ -23,4 +23,67 @@ module.exports = class PhoneRegister {
     }
     return types;
   }
-};
+
+  getNumbersByType(firstname, lastname, type) {
+    if (firstname && lastname && type) {
+      const numbersFound = [];
+      for (let person of this.phoneRegister) {
+        if (person.firstname === firstname && person.lastname === lastname) {
+          for (let phone of person.phones) {
+            if (phone.type === type) {
+              numbersFound.push(phone.number);
+            }
+          }
+        }
+      }
+      return numbersFound;
+    } else {
+      throw new Error("missing parameter");
+    }
+  }
+
+  getAllNumbersByType(type) {
+    if (!type) throw new Error("missing parameter");
+    const numbersFound = [];
+
+    for (let person of this.phoneRegister) {
+      for (let phone of person.phones) {
+        if (phone.type === type) {
+          numbersFound.push({
+            firstname: person.firstname,
+            lastname: person.lastname,
+            number: {
+              type: phone.type,
+              tel: phone.number,
+            },
+          });
+        }
+      }
+    }
+    return numbersFound;
+  }
+
+  getAllNumbers() {
+    const found = [];
+    for (let person of this.phoneRegister) {
+      if (person.phones && person.phones.length > 0) {
+        found.push(person);
+      }
+    }
+    return found;
+  }
+
+  getName(number) {
+    for (let person of this.phoneRegister) {
+      for (let phone of person.phones) {
+        if (phone.number === number) {
+          return {
+            firstname: person.firstname,
+            lastname: person.lastname,
+          };
+        }
+      }
+    }
+    return null;
+  }
+}; //end of class
